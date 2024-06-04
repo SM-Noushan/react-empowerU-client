4fadcb39a94c4e00938b6a03ba124282c8c2fc91
SMNoushan <noushan.md@gmail.com>
2024-06-04 16:32:29 +0600

    added error page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/home/Home";
+import Error from "../components/shared/Error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-    errorElement: "<Error />",
+    errorElement: <Error />,
     children: [
       {
         index: true,
