ecfeb87b670a7cc4c98f5d06fdbd3a6f45796d7d
SMNoushan <noushan.md@gmail.com>
2024-06-06 23:42:59 +0600

    added firebase config

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,10 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/home/Home";
 import Error from "../components/shared/Error";
-// import SignIn from "../components/auth/SignIn";
-// import SignUp from "../components/auth/SignUp";
 import Auth from "../components/auth/Auth";
-import SignUp from "../components/auth/SignUp";
 
 export const router = createBrowserRouter([
   {
