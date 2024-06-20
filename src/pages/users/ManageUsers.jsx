d52e2ea3401cfd2448ebfd186e027604d89b1783
SMNoushan <noushan.md@gmail.com>
2024-06-20 13:00:03 +0600

    updated manage users (added delete user)

--- src/pages/users/ManageUsers.jsx
+++ src/pages/users/ManageUsers.jsx
@@ -8,15 +8,41 @@ import PopUpModal from "../../components/PopUpModal";
 import useFetchData from "../../hooks/useFetchData";
 import useAuth from "../../hooks/useAuth";
 import { FaTrashCan } from "react-icons/fa6";
+import { useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import usePostData from "../../hooks/usePostData";
+import { toast } from "react-toastify";
 
 const ManageUsers = () => {
   const { user } = useAuth();
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [selectedUserId, setSelectedUserId] = useState("");
   const { data, isLoading } = useFetchData(
     "users",
     `users?uid=${user.uid}`,
     {},
     true
   );
+  const queryClient = useQueryClient();
+  const { mutateAsync: deleteUserMutation } = usePostData();
+
+  const handleDelete = async (id) => {
+    const obj = {
+      method: "delete",
+      url: `users/${id}?uid=${user.uid}`,
+    };
+    try {
+      const resDB = await deleteUserMutation(obj);
+      if (resDB.data?.deletedCount) {
+        setDeleteModal(false);
+        queryClient.invalidateQueries(["users"]);
+        return toast.success("User Removed");
+      }
+    } catch (error) {
+      // console.log(error);
+      return toast.error("Failed! Try again");
+    }
+  };
   return (
     <DashboardContainer>
       <Helmet>
@@ -56,10 +82,13 @@ const ManageUsers = () => {
                     {user.name}
                   </Table.Cell>
                   <Table.Cell>{user.email}</Table.Cell>
-                  <Table.Cell>{user.role}</Table.Cell>
+                  <Table.Cell className="uppercase">{user.role}</Table.Cell>
                   <Table.Cell>
                     <button
-                      //   onClick={() => setDeleteModal(true)}
+                      onClick={() => {
+                        setSelectedUserId(user._id);
+                        setDeleteModal(true);
+                      }}
                       className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
                     >
                       <FaTrashCan />
@@ -73,11 +102,12 @@ const ManageUsers = () => {
       </div>
 
       {/* confirm delete modal */}
-      {/* <PopUpModal
-      // modalState={deleteModal}
-      // toggleModal={setDeleteModal}
-      // onClick={() => handleDelete(deleteId)}
-      /> */}
+      <PopUpModal
+        modalState={deleteModal}
+        toggleModal={setDeleteModal}
+        onClick={() => handleDelete(selectedUserId)}
+        typeDelete={true}
+      />
     </DashboardContainer>
   );
 };
